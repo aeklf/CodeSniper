0550b5ad904aca6cfa890465909e980c86d3bd40
macosta2015 <macosta2015@my.fit.edu>
2022-08-08 19:07:46 -0400

    Adding the Front End initial code.

---
+++ client/src/App.jsx
@@ -0,0 +1,47 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Topbar from "./components/topbar/Topbar";
+import LogInSignUp from "./components/logInSignUp/LogInSignUp";
+
+import Homepage from "./pages/homepage/Homepage";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  const currentUser = true;
+  return (
+    <Router>
+      {/* <Topbar /> */}
+      <LogInSignUp />
+      {/* <LogInSignUp /> */}
+
+      {/* <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route path="/posts">
+          <Homepage />
+        </Route>
+        <Route path="/register">
+          {currentUser ? <Homepage /> : <Register />}
+        </Route>
+        <Route path="/login">{currentUser ? <Homepage /> : <Login />}</Route>
+        <Route path="/post/:id">
+          <Single />
+        </Route>
+        <Route path="/write">{currentUser ? <Write /> : <Login />}</Route>
+        <Route path="/settings">
+          {currentUser ? <Settings /> : <Login />}
+        </Route>
+      </Switch> */}
+    </Router>
+  );
+}
+
+export default App;
