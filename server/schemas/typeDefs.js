c09eedc12a695d62ff32abd6e08be08510d969df
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-08-08 19:56:26 -0500

    Updated graphql typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,10 +3,25 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type Profile {
     _id: ID
-    name: String
+    username: String
+    email: String
+    userPosts: [Post]
+  }
+  
+  type Post {
+    _id: ID
+    title: String
+    topic: String
+    content: String
+  }
+  
+  type Auth {
+    token: ID!
+    profile: Profile
   }
 
   type Query {
+    me: Profile
     profiles: [Profile]!
     profile(profileId: ID!): Profile
   }
