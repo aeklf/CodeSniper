594a166a56adbe4226854211c3c79b266b64c190
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-08-08 19:40:02 -0500

    Updated models

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,31 +1,20 @@
 const { Schema, model } = require('mongoose');
 
-const postSchema = new Schema({
-  _id: {
-    type: _id,
-    required: true,
-    unique: true,
-  },
-  title: [
-    {
-      type: String,
-      required: true,
-    },
-  ],
-  topic: [
+const postSchema = new Schema(
   {
-    type: String,
-    required: true,
-  },
-  ],
-  content: [
-    {
+    title: {
       type: String,
       required: true,
     },
-  ],
-});
+    topic: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  }
+);
 
-const Post = model('Post', postSchema);
-
-module.exports = Post;
+module.exports = postSchema;
--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,18 +1,42 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+const postSchema = require('./Post');
 
-const profileSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-});
+const profileSchema = new Schema(
+    {
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must use a valid email address'],
+      },
+      password : {
+        type: String,
+        required: true,
+      },
+        userPosts: [postSchema],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      }
+    }
+);
+
+//hash user password
+profileSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+})
 
 const Profile = model('Profile', profileSchema);
 
