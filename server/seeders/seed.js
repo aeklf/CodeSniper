a31ba7de667b685d22984a006f6a416e83b6eab1
Daniel Olmedo <olmedo.daniel96@gmail.com>
2022-08-08 19:49:49 -0500

    Corrected Apollo server paths

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,16 +1,12 @@
 // Seeding script
 const db = require('../config/connection');
 const { Profile } = require('../models');
-const { Post } = require('../models');
 const profileSeeds = require('./profileSeeds.json');
-const postSeeds = require('./postSeeds.json');
 
 db.once('open', async () => {
     try {
       await Profile.deleteMany({});
       await Profile.create(profileSeeds);
-      await Post.deleteMany({});
-      await Post.create(postSeeds);
   
       console.log('all done!');
       process.exit(0);
