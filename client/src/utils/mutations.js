a30dd3982301d5e67b19d77b2f8498cef0763054
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-08-12 14:02:33 -0500

    Added LOGIN_USER mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,14 +1,33 @@
 import { gql } from "@apollo/client";
 
 export const ADD_PROFILE = gql`
-mutation addProfile($username: String!, $email: String!, $password: String!) {
-    addProfile(username: $username, email: $email, password: $password) {
-      token
-      user {
-        email
-        username
-        _id
-      }
+    mutation addProfile($username: String!, $email: String!, $password: String!) {
+        addProfile(username: $username, email: $email, password: $password) {
+          token
+          user {
+            email
+            username
+            _id
+          }
+        }
     }
-  }
-`
+`;
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+                userPosts {
+                    _id
+                    title
+                    topic
+                    content
+                }
+            }
+        }
+    }
+`;
