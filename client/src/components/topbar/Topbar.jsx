0b4e1ccda1f711e9c332a9b4f81f70698c3073ad
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-08-13 12:53:26 -0500

    New topbar version

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FaPlusCircle } from "react-icons/fa";
 import "./topbar.css";
 import Auth from "../../utils/auth";
 
