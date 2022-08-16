08518954437a8813b2bb5827a217b74160f6c0f4
Daniel Olmedo <olmedo.daniel96@gmail.com>
2022-08-15 19:54:30 -0500

    Added route for writing posts

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -1,11 +1,41 @@
 import "./write.css";
+import React, {useState} from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import { useMutation } from "@apollo/client";
+import { WRITE_POST } from '../../utils/mutations';
 
+const WritePost = () => {
 
-export default function Write() {
+  const [postData, setPostData] = useState({ title:'', topic:'', content:''})
+  const [createPost, {error}] = useMutation(WRITE_POST);
+
+  const handleInputChange = (event) => {
+    const {name, value} = event.target;
+    setPostData({...postData, [name]:value})
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    try {
+      const {data} = await createPost({
+        variables: postData
+      });
+    }catch (err) {
+      console.log(err)
+    }
+
+    setPostData({
+      title:'',
+      topic:'',
+      content:'',
+    });
+  }
+
   return (
     <Container fluid>
       <Row>
@@ -31,52 +61,32 @@ export default function Write() {
           <Col id="TextBox" md={{ span: 6, offset: 3 }}>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Title: </Form.Label>
-              <Form.Control type="email" placeholder="...type the code" />
+              <Form.Control type='text' name="title" placeholder="...type the code" onChange={handleInputChange} value={postData.title}/>
             </Form.Group>
           </Col>
           
           <Col id="TextBox" md={{ span: 6, offset: 3 }}>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Topic: </Form.Label>
-              <Form.Control type="topic" placeholder="... web development" />
+              <Form.Control type='text' name="topic" placeholder="... web development" onChange={handleInputChange} value={postData.topic}/>
             </Form.Group>
           </Col>
 
           <Col md={{ span: 6, offset: 3 }}>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Type your code here: </Form.Label>
-              <Form.Control as="textarea" placeholder="console.log('Hello Mexico')" rows={3} />            
+              <Form.Control as="textarea" type='text' name="content" placeholder="console.log('Hello Mexico')" rows={3} onChange={handleInputChange} value={postData.content}/>            
             </Form.Group>
           </Col>
+          <button type='submit' className="submitButton" onClick={handleFormSubmit}>Post</button>
         </Form> 
 
       <Row>
-          {/* <form className="writeForm">
-            <div className="writeFormGroup">
-              <label htmlFor="fileInput">
-              </label>
-              <input id="fileInput" type="file" style={{ display: "none" }} />
-              <input
-                className="writeInput"
-                placeholder="Title"
-                type="text"
-                autoFocus={true}
-              />
-            </div>
-            <div className="writeFormGroup">
-              <textarea
-                className="writeInput writeText"
-                placeholder="Tell your story..."
-                type="text"
-                autoFocus={true}
-              />
-            </div>
-            <button className="writeSubmit" type="submit">
-              Publish
-            </button>
-          </form> */}
+
       </Row>
     
     </Container>
   );
 }
+
+export default WritePost;
