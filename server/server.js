339350a3a470969867552f0990ec4a371cfaabb3
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-08-19 11:12:22 -0500

    Fix for the deployment issue

--- server/server.js
+++ server/server.js
@@ -21,9 +21,13 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+})
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
