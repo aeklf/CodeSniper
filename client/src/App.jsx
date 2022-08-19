63c22363a1c3054872d7e756d1193937c8f1203b
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-08-19 10:56:58 -0500

    Removed comments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,12 +15,7 @@ import Topbar from "./components/topbar/Topbar";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
 import Settings from "./pages/settings/Settings";
-import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
-
-//Worng old routes 
-// import Register from "./pages/register/Register";
-// import Login from "./pages/login/Login";
 
 //Proper register/login page
 import Register from "./pages/register/Register";
@@ -50,13 +45,10 @@ const client = new ApolloClient({
 });
 
 function App() {
-  const currentUser = true;
   return (
     <ApolloProvider client = { client }>
       <Router>
-        {/* Components */}
          <Topbar />
-
         <Routes>
           <Route
             path='/'
@@ -79,30 +71,6 @@ function App() {
             element={<Settings />}
           />
         </Routes>
-        
-
-        {/*<Login />*/}
-        {/*<Register />*/}
-
-        {/*{ <Switch>*/}
-        {/*  <Route exact path="/">*/}
-        {/*    <Homepage />*/}
-        {/*  </Route>*/}
-        {/*  <Route path="/posts">*/}
-        {/*    <Homepage />*/}
-        {/*  </Route>*/}
-        {/*  <Route path="/register">*/}
-        {/*    {currentUser ? <Homepage /> : <Register />}*/}
-        {/*  </Route>*/}
-        {/*  <Route path="/login">{currentUser ? <Homepage /> : <Login />}</Route>*/}
-        {/*  <Route path="/post/:id">*/}
-        {/*    <Single />*/}
-        {/*  </Route>*/}
-        {/*  <Route path="/write">{currentUser ? <Write /> : <Login />}</Route>*/}
-        {/*  <Route path="/settings">*/}
-        {/*    {currentUser ? <Settings /> : <Login />}*/}
-        {/*  </Route>*/}
-        {/*</Switch> }*/}
       </Router>
     </ApolloProvider>
   );
